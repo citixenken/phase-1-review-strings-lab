25bf35310f08337c1f07d0407677f260e12f4d30
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-06-09 17:02:23 +0300

    commit tree 5c3b0aaf7f09d4df391dd8b5b787041358f62f20

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+let currentUser = "Ken Muyesu";
+// let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`; //using string interpolation
+
+// let excitedWelcomeMessage =
+//   "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+// let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`; //using string interpolation
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// let shortGreeting = "Welcome, " + currentUser[0] + "!";
+// let shortGreeting = `Welcome, ${currentUser[0]}!`; //using string interpolation
+let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`; //using string interpolation
